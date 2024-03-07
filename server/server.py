003e421e7d120783575eba3605e2967b28ad5272
Aym-Aym <33794755+Aym-Aym@users.noreply.github.com>
2024-03-06 21:45:13 -0800

    Refactor code and add custom exceptions

--- server/server.py
+++ server/server.py
@@ -2,6 +2,8 @@ import socket
 from threading import Thread
 from datetime import datetime
 from monitor import Monitor
+from exceptions import ReadClientException, SendClientException
+from typing import Union
 
 
 class Server:
@@ -12,10 +14,10 @@ class Server:
     More to come...
     """
 
-    def __init__(self):
+    def __init__(self) -> None:
         super(Server, self).__init__()
 
-        """ Initialize default vars """
+        # Initialize default vars.
 
         self.HEADER = 10
         self.SERVER_IP = "127.0.0.1"
@@ -24,18 +26,22 @@ class Server:
         self.FORMAT = "utf-8"
         self.client_disconnect = "!disconnect"
         self.server_password = "password123"
+        self.SERVER_STARTED = False
 
-        """ Create server """
-
+        # Create socket server.
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind(self.address)
 
         self.clients_dict = dict()
 
+        # Start server.
         self.start()
 
-    def start(self):
+    def start(self) -> None:
+        """Starts the server."""
+        self.SERVER_STARTED = True
+
         date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         message = f"[srv]  "\
                   f"[{date}]  "\
@@ -52,7 +58,9 @@ class Server:
         print(message)
         self.logs(message)
 
-        while True:
+        # Waiting for client to connect and send commands.
+        while self.SERVER_STARTED:
+            # Get the client socket object and client address (ip, port).
             client_socket, client_address = self.server.accept()
 
             self.clients_dict[client_socket] = list()
@@ -70,37 +78,51 @@ class Server:
 
             self.send(client_socket, message)
 
-            """ Authenticating client """
-
+            # Authenticating client.
             auth = self.authentication(client_socket=client_socket)
+            # If auth failed, disconnect client.
             if not auth:
                 reason = "failed to authenticate"
-                self.disconnect(client_socket=False, client_address=client_address, message=reason)
+                self.disconnect(
+                    client_socket=False,
+                    client_address=client_address,
+                    message=reason
+                )
                 self.stats()
                 client_socket.close()
                 continue
             else:
-                message = f"[srv]  " \
+                message = "[srv]  " \
                           f"[{date}]  " \
                           f"[client {client_address[0]}:{client_address[1]}]  " \
-                          f"authentication succeed..."
+                          "authentication succeed..."
                 print(message)
                 self.logs(message)
                 self.stats()
 
             self.client(client_socket, client_address)
 
-    def client(self, client_socket, client_address):
+    def client(self, client_socket: socket.socket, client_address: tuple) -> None:
+        """Deals with sending clients requested datas.
+
+        Args:
+            client_socket (socket.socket): Client socket object.
+            client_address (tuple): Client address in format (IP, PORT).
+        """
         connected = True
 
         message = "you are now connected..."
         self.send(client_socket, message)
 
-        thread_read = Thread(target=self.thread, args=(client_socket, client_address))
+        thread_read = Thread(
+            target=self.thread,
+            args=(client_socket, client_address)
+        )
         thread_read.start()
 
         monitor = Monitor(1)
 
+        # While the client stays connected.
         while connected:
             connected = thread_read.is_alive()
             if client_socket in self.clients_dict.keys():
@@ -118,7 +140,13 @@ class Server:
 
                     self.send(client_socket, message=to_send, log=False)
 
-    def thread(self, client_socket, client_address):
+    def thread(self, client_socket: socket.socket, client_address: tuple) -> None:
+        """Create a thread to receive client requests.
+
+        Args:
+            client_socket (socket.socket): Client socket object.
+            client_address (tuple): Client address in format (IP, PORT).
+        """
         while True:
             command = self.read(client_socket)
 
@@ -131,13 +159,15 @@ class Server:
                 break
 
             date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            message = f"[rcv]  "\
+            message = "[rcv]  "\
                       f"[{date}]  "\
                       f"[client {client_address[0]}:{client_address[1]}]  "\
                       f"command: {command}"
             print(message)
             self.logs(message)
 
+            # Check commands received from the client.
+            # And update the clients dict that contains the list of data to send.
             if command == "!disconnect":
                 message = "client request"
                 self.disconnect(client_socket, client_address, message)
@@ -186,12 +216,17 @@ class Server:
                 print(message)
                 self.logs(message)
 
-                message = f"stopping all send..."
+                message = "stopping all send..."
                 self.send(client_socket, message)
             else:
                 self.send(client_socket, "unknown command...")
 
-    def read(self, client_socket):
+    def read(self, client_socket: socket.socket) -> Union[str, bool]:
+        """Reads requests from client.
+
+        Args:
+            client_socket (socket.socket): Client socket object.
+        """
         try:
             message_size = client_socket.recv(self.HEADER)
             message_size = int(message_size)
@@ -203,35 +238,65 @@ class Server:
 
             return str(message)
 
-        except:
+        except ReadClientException:
             return False
 
-    def send(self, client_socket, message, who="server response", log=True):
+    def send(
+        self,
+        client_socket: socket.socket,
+        message: str,
+        who: str = "server response",
+        log: bool = True
+    ) -> bool:
+        """Sending data to client.
+
+        Args:
+            client_socket (socket.socket): Client socket object.
+            message (str): Message to send to the client.
+            who (str): Who is sending the message.
+            log (bool): Are we loggin the message.
+
+        Returns:
+            bool: True if message was sent else False.
+        """
         try:
             if who == "server response":
                 who = f"server {self.SERVER_IP}:{self.PORT}"
+
             message = str(message)
             date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             message_lines = message.split("\n")
 
-            new_message_lines = [f"[{date}]  [{who}]  {message}" for message in message_lines]
+            new_message_lines = [
+                f"[{date}]  [{who}]  {message}"
+                for message in message_lines
+            ]
             join_with = "\n"
             new_message = join_with.join(new_message_lines)
 
             if log:
                 self.logs(f"[snd]  {new_message}")
 
+            # Encoding the message then send it.
             message = new_message.encode("utf-8")
             message_header = f"{len(message):<{self.HEADER}}".encode("utf-8")
             client_socket.send(message_header + message)
 
             return True
 
-        except:
+        except SendClientException:
             return False
 
-    def authentication(self, client_socket):
-        password = self.read(client_socket)
+    def authentication(self, client_socket: socket.socket) -> bool:
+        """Authenticate the client.
+
+        Args:
+            client_socket (socket.socket): Client socket object.
+
+        Returns:
+            bool: True if the client is authenticated else False.
+        """
+        password = self.read(client_socket)  # Read password sent by the client.
 
         if password == self.server_password:
             message = "authentication succeeded..."
@@ -247,7 +312,19 @@ class Server:
 
             return False
 
-    def disconnect(self, client_socket, client_address, message):
+    def disconnect(
+        self,
+        client_socket: socket.socket,
+        client_address: tuple,
+        message: str
+    ) -> None:
+        """Disconnecting the client.
+
+        Args:
+            client_socket (socket.socket): Client socket object.
+            client_address (tuple): Client address in format (IP, PORT).
+            message (str): Tell client that he has been disconnected.
+        """
         date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         if client_socket:
             message_send = f"disconnected from server {self.SERVER_IP}..."
@@ -261,7 +338,8 @@ class Server:
         print(message)
         self.logs(message)
 
-    def stats(self):
+    def stats(self) -> None:
+        """Print server stats."""
         date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         message = f"[srv]  "\
                   f"[{date}]  "\
